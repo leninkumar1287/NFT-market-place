54b5413c7c35864b7fba491d1f335983d5f548d8
leninkumar1287 <leninkumar.vadivel@neosoftmail.com>
2022-10-13 14:14:45 +0530

    backupCommit

--- migrations/2_ERC721AuctionMigration.js
+++ migrations/2_ERC721AuctionMigration.js
@@ -1,5 +1,4 @@
 const ERC721Auction = artifacts.require("ERC721Auction");
-const web3 = require('web3')
 
 module.exports = function (deployer) {
   deployer.deploy(ERC721Auction, "0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2", 123, 2);
--- test/ERC721AuctionTest.js
+++ test/ERC721AuctionTest.js
@@ -1,5 +1,4 @@
 const ERC721Auction = artifacts.require("ERC721Auction")
-var utils = require('ethers').utils;
 
 contract("ERC721Auction", (accounts) => {
     let bidder1 = accounts[1];
@@ -11,6 +10,7 @@ contract("ERC721Auction", (accounts) => {
 
     it("Contract should be deployed", async () => {
         ERC721Token = await ERC721Auction.deployed();
+        console.log("ERC721Token : ", ERC721Token)
         assert(ERC721Token.address)
     })
 
@@ -21,9 +21,11 @@ contract("ERC721Auction", (accounts) => {
     })
 
     it("User can bid and checking with heighest bidder", async () => {
-        await ERC721Token.bid({ from : bidder1, value: 3 });
+        let v = await ERC721Token.bid({ from : bidder1, value: 3 });
         await ERC721Token.bid({ from : bidder2, value: 4 });
         let HeighestBidder = await ERC721Token.viewHeigestBid();
+        console.log(" this is out  : ", v); 
+        console.log(" this is out  : ", HeighestBidder);
         assert(HeighestBidder == bidder2);
     })
 
@@ -31,19 +33,17 @@ contract("ERC721Auction", (accounts) => {
         let beforeEnd = await ERC721Token.activeStatuOfAuction();
         await ERC721Token.end();
         let afterEnd = await ERC721Token.activeStatuOfAuction();
+        console.log(" this is out  : ", afterEnd);
         assert(beforeEnd == (!afterEnd));
     })
     
     it("if your lose the auction then bids can be withdraw after the auction end ", async () => {
+        // actual bid value is 3
+        let adder = BigInt(3)
         let requester= accounts[1];
         let beforeWithdraw = await ERC721Token.balance(requester);
-        let beforeValue = BigInt(beforeWithdraw).toString();
         await ERC721Token.withdraw(requester);
         let afterWithdraw = await ERC721Token.balance(requester);
-        let afterValue = BigInt(afterWithdraw).toString();
-        console.log("",beforeValue,"\n",afterValue.length)
-        var wei = utils.bigNumberify(beforeValue+4);
-        console.log("\n",wei.toString())
-        assert(BigInt(beforeWithdraw).toString() == BigInt(afterWithdraw).toString());
+        assert(BigInt(beforeWithdraw)+adder == BigInt(afterWithdraw));
     })
 })
